e361e219d245d1aef7f5db2af24e7858baa36a1c
thsghdud13 <thsgdud25@naver.com>
2024-11-30 02:10:32 +0900

    feat: 증정 이벤트 기능

--- src/main/java/christmas/model/domain/promotion/PromotionPolicy.java
+++ src/main/java/christmas/model/domain/promotion/PromotionPolicy.java
@@ -1,7 +1,31 @@
 package christmas.model.domain.promotion;
 
+import christmas.model.domain.EventValidator;
 import christmas.model.domain.Reservation;
+import christmas.model.domain.menu.Menu;
+import christmas.model.vo.Money;
+import java.time.LocalDate;
 
-public interface PromotionPolicy {
-    Promotion getPromotion(Reservation reservation);
+public class PromotionPolicy {
+    private final LocalDate startDate = LocalDate.of(2023, 12, 1);
+    private final LocalDate endDate = LocalDate.of(2023, 12, 31);
+    private final Money criteria = new Money(120000);
+    private final Menu promotionMenu;
+    private final int promotionCount;
+
+    public PromotionPolicy(Menu promotionMenu, int promotionCount) {
+        this.promotionMenu = promotionMenu;
+        this.promotionCount = promotionCount;
+    }
+
+    public Promotion getPromotion(Reservation reservation) {
+        if (!EventValidator.validate(startDate, endDate, reservation)) {
+            return new Promotion(promotionMenu, 0);
+        }
+        Money totalOrderMoney = reservation.getTotalOrderMoney();
+        if (totalOrderMoney.compareTo(criteria) >= 0) {
+            return new Promotion(promotionMenu, promotionCount);
+        }
+        return new Promotion(promotionMenu, 0);
+    }
 }
