29767ffae11ed278aa0cf4de225ea39a06088ff5
thsghdud13 <thsgdud25@naver.com>
2024-11-29 21:10:58 +0900

    feat: 뱃지 기능 구현

--- src/main/java/christmas/model/domain/Badge.java
+++ src/main/java/christmas/model/domain/Badge.java
@@ -1,2 +1,28 @@
-package christmas.model.domain;public class Badge {
+package christmas.model.domain;
+
+import christmas.model.vo.Money;
+
+public enum Badge {
+    NONE("없음", new Money(0), new Money(4999)),
+    STAR("별", new Money(5000), new Money(9999)),
+    TREE("트리", new Money(10000), new Money(49999)),
+    SANTA("산타", new Money(50000), Money.max());
+    private String name;
+    private Money minimumPrice;
+    private Money maximumPrice;
+
+    Badge(String name, Money minimumPrice, Money maximumPrice) {
+        this.name = name;
+        this.minimumPrice = minimumPrice;
+        this.maximumPrice = maximumPrice;
+    }
+
+    public static Badge getBadge(Money money) {
+        for (Badge badge : values()) {
+            if (badge.maximumPrice.compareTo(money) >= 0 && money.compareTo(badge.minimumPrice) >= 0) {
+                return badge;
+            }
+        }
+        return NONE;
+    }
 }
