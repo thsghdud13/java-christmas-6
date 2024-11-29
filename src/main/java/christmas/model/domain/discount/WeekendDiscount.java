f652f1702cf9b3e517d4548f60040bcb9990a444
thsghdud13 <thsgdud25@naver.com>
2024-11-30 01:16:22 +0900

    feat: 주말 할인 기능 추가

---
+++ src/main/java/christmas/model/domain/discount/WeekendDiscount.java
@@ -0,0 +1,37 @@
+package christmas.model.domain.discount;
+
+import christmas.model.domain.OrderItem;
+import christmas.model.domain.Reservation;
+import christmas.model.domain.menu.MenuType;
+import christmas.model.vo.Money;
+import java.time.LocalDate;
+import java.util.List;
+
+public class WeekendDiscount implements DiscountPolicy {
+    private final LocalDate startDate = LocalDate.of(2023, 12, 1);
+    private final LocalDate endDate = LocalDate.of(2023, 12, 31);
+    private final Money discountAmount = new Money(2023);
+
+    @Override
+    public Discount getDiscountAmount(Reservation reservation) {
+        LocalDate reserveDate = reservation.getReserveDate();
+        if (reserveDate.isBefore(startDate) || reserveDate.isAfter(endDate)) {
+            return new Discount(DiscountType.WEEKEND, new Money(0));
+        }
+        if (reserveDate.getDayOfWeek().getValue() <= 4 || reserveDate.getDayOfWeek().getValue() == 7) {
+            return new Discount(DiscountType.WEEKEND, new Money(0));
+        }
+        int mainCount = getMainCount(reservation.getOrderItems());
+        return new Discount(DiscountType.WEEKEND, discountAmount.multiply(mainCount));
+    }
+
+    private int getMainCount(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .mapToInt(o -> {
+                    if (o.getMenuType().equals(MenuType.MAIN)) {
+                        return o.getCount();
+                    }
+                    return 0;
+                }).sum();
+    }
+}
---
+++ src/test/java/christmas/model/domain/discount/WeekendDiscountTest.java
@@ -0,0 +1,34 @@
+package christmas.model.domain.discount;
+
+import christmas.model.domain.OrderItem;
+import christmas.model.domain.Reservation;
+import christmas.model.domain.menu.Menu;
+import christmas.model.domain.menu.MenuType;
+import christmas.model.vo.Money;
+import java.time.LocalDate;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class WeekendDiscountTest {
+    WeekendDiscount weekendDiscount = new WeekendDiscount();
+
+    @ParameterizedTest
+    @CsvSource(value = {"2023,12,7,2,0", "2023,12,3,1,0", "2023,12,8,2,4046", "2023,10,8,23,0", "2024,1,1,30,0"})
+    public void 평일_할인_테스트(int year, int month, int dayOfMonth, int dessertCount, int result) {
+        //given
+        LocalDate testDate = LocalDate.of(year, month, dayOfMonth);
+        Menu menu = new Menu(MenuType.MAIN, new Money(1000), "케이크");
+        OrderItem orderItem = new OrderItem(menu, dessertCount);
+        Reservation reservation = new Reservation(null, null, List.of(orderItem), testDate);
+
+        //when
+        Discount discount = weekendDiscount.getDiscountAmount(reservation);
+
+        //then
+        Assertions.assertThat(new Money(result))
+                .isEqualTo(discount.getDiscountAmount());
+    }
+
+}
