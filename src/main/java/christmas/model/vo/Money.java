86d57b881cc3e9b207d5746a88163d272d6943da
thsghdud13 <thsgdud25@naver.com>
2024-11-29 21:10:24 +0900

    feat: 연산 메서드 Money에 추가

--- src/main/java/christmas/model/vo/Money.java
+++ src/main/java/christmas/model/vo/Money.java
@@ -1,4 +1,35 @@
 package christmas.model.vo;
 
-public class Money {
+public record Money(int value) implements Comparable<Money> {
+
+    public Money {
+        validateValue(value);
+    }
+
+    private void validateValue(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("돈은 양수여야 함");
+        }
+    }
+
+    public static Money max() {
+        return new Money(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public int compareTo(Money o) {
+        return Integer.compare(this.value, o.value);
+    }
+
+    public Money multiply(int count) {
+        return new Money(value * count);
+    }
+
+    public Money add(Money money) {
+        return new Money(value + money.value());
+    }
+
+    public Money sub(Money money) {
+        return new Money(value - money.value());
+    }
 }
