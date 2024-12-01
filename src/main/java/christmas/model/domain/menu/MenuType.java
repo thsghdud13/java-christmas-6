6c9cacf66d443f2ef35aa3e82d3ae01ece0e0d26
thsghdud13 <thsgdud25@naver.com>
2024-12-01 16:34:06 +0900

    feat: 메뉴 이름 기반으로 생성하는 기능

--- src/main/java/christmas/model/domain/menu/MenuType.java
+++ src/main/java/christmas/model/domain/menu/MenuType.java
@@ -5,4 +5,20 @@ public enum MenuType {
     MAIN,
     DESSERT,
     BEVERAGE;
+
+    public static MenuType of(String typeName) {
+        if (typeName.equals("에피타이저")) {
+            return APPETIZER;
+        }
+        if (typeName.equals("메인")) {
+            return MAIN;
+        }
+        if (typeName.equals("디저트")) {
+            return DESSERT;
+        }
+        if (typeName.equals("음료")) {
+            return BEVERAGE;
+        }
+        throw new IllegalArgumentException("그런 메뉴 타입은 없음");
+    }
 }
