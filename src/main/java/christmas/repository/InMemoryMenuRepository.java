79388b92cb3b9723f50f21cf5840e09cf10ad310
thsghdud13 <thsgdud25@naver.com>
2024-12-01 16:32:16 +0900

    feat: 메뉴 파일로부터 메뉴 정보 레포지토리에 저장하는 기능 추가
    * save 함수와 clear함수도 삭제

--- src/main/java/christmas/repository/InMemoryMenuRepository.java
+++ src/main/java/christmas/repository/InMemoryMenuRepository.java
@@ -1,25 +1,34 @@
 package christmas.repository;
 
 import christmas.model.domain.menu.Menu;
+import christmas.model.domain.menu.MenuType;
+import christmas.model.vo.Money;
+import christmas.util.CSVData;
+import christmas.util.CSVFileLoader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public class InMemoryMenuRepository implements MenuRepository {
     private final HashMap<String, Menu> menuHashMap = new HashMap<>();
+    private final String menuInfoFilePath = "src/main/resources/menus.md";
+    private static final InMemoryMenuRepository INSTANCE = new InMemoryMenuRepository();
 
-    @Override
-    public void save(Menu menu) {
-        menuHashMap.put(menu.getName(), menu);
+    private InMemoryMenuRepository() {
+        CSVData menuData = CSVFileLoader.readCSV(menuInfoFilePath);
+        for (List<String> row : menuData.getRows()) {
+            menuHashMap.put(row.get(1),
+                    new Menu(MenuType.of(row.get(0)), new Money(Integer.parseInt(row.get(2))), row.get(1)));
+        }
+    }
+
+    public static InMemoryMenuRepository getInstance() {
+        return INSTANCE;
     }
 
     @Override
     public Optional<Menu> findByName(String name) {
         return Optional.of(menuHashMap.get(name));
-    }
-
-    @Override
-    public void clear() {
-        menuHashMap.clear();
     }
 
 }
--- src/main/java/christmas/repository/MenuRepository.java
+++ src/main/java/christmas/repository/MenuRepository.java
@@ -4,9 +4,5 @@ import christmas.model.domain.menu.Menu;
 import java.util.Optional;
 
 public interface MenuRepository {
-    void save(Menu menu);
-
     Optional<Menu> findByName(String name);
-
-    void clear();
 }
