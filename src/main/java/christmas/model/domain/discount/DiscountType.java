73245b621c06da418b46dfbc57abba2654042279
thsghdud13 <thsgdud25@naver.com>
2024-12-01 20:50:55 +0900

    feat: DiscountType에 이름 추가

--- src/main/java/christmas/model/domain/discount/DiscountType.java
+++ src/main/java/christmas/model/domain/discount/DiscountType.java
@@ -1,8 +1,17 @@
 package christmas.model.domain.discount;
 
 public enum DiscountType {
-    CHRISTMAS,
-    WEEKDAY,
-    WEEKEND,
-    SPECIAL
+    CHRISTMAS("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인");
+    private final String name;
+
+    DiscountType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
